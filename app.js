911c476636a8dc9659287ba7f66b56ac8c0c2a0b
Ivan Luviano <sixtosivan0@gmail.com>
2025-08-04 20:20:44 -0600

    Trabajando en app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosList = [];
+
+function AgregarAmigo(){
+    let nombres = document.getElementById('amigo');
+    let amigos = nombres.value.trim();
+
+    if(amigos == ""){
+        alert("Agrega un nombre");
+        return;
+    }
+amigosList.push(amigos);
+nombres.value = "";
+
+amigosList();
+}
+function amigosList(){
+let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = ""; // Limpiar lista previa
+
+    amigosList.forEach((amigo, index) => {
+        let li = document.createElement("li");
+        li.textContent = `${index + 1}. ${amigo}`;
+        ul.appendChild(li);
+  });}
